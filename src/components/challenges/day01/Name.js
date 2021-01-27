cbfe71dc2c3234ecc32f319368e2962f4dcc01cd
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-01-26 21:21:11 -0500

    Created components

---
+++ src/components/challenges/day01/Name.js
@@ -0,0 +1,16 @@
+const Name = () => {
+    return (
+        <div>
+        <h1>Perla</h1>
+        <p>My name is Perla and I am learning to use React.</p>
+        <ul>
+            <li>I like to eat Bacon.</li>
+            <li>I have a dog named Mika.</li>
+            <li>I drive a Tesla.</li>
+            </ul>
+        </div>
+        
+    )
+};
+
+export default Name;
