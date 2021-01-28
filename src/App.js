6037e48d9cbebd94d8734728c0d00aaac2429852
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-01-28 18:23:41 -0500

    new app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
-import Name from './components/challenges/day01/Name'
-import Footer from './components/challenges/day01/Footer'
-import Home from './components/challenges/day01/Home'
+// import Header from './components/Header'
+// import Name from './components/challenges/day01/Name'
+// import Footer from './components/challenges/day01/Footer'
+// import Home from './components/challenges/day01/Home'
 
-function App() { //this is a root component
-  const name = 'Perla' //JS above return statement can be
-                      // injected into JSX using {}
+// function App() { //this is a root component
+//   const name = 'Perla' //JS above return statement can be
+//                       // injected into JSX using {}
 
+//   return (
+//     <div className="App">
+//       <h1>Welcome to React, {name}</h1>
+//       <h2>We just modified our root App Component</h2>
+//       <h3>CHALLENGE!</h3>
+//       <p>Create ./components/challenges/day 01 folder</p>
+//       <p>Create a component with h1 tag of your name, p tag with description  of yourself, and an unordered list of two truths and a lie.</p>
+//       <p>Export, import into App, and mount it.</p>
+//       <p>Deadline at 8:05pm</p>
+//       <Header /> {/* this is how you mount a component */}
+//       <Name />
+//       <Footer />
+//       <Home />
+//     </div>
+    
+//   );
+// }
+
+// export default App;
+
+function App() {
   return (
     <div className="App">
-      <h1>Welcome to React, {name}</h1>
-      <h2>We just modified our root App Component</h2>
-      <h3>CHALLENGE!</h3>
-      <p>Create ./components/challenges/day 01 folder</p>
-      <p>Create a component with h1 tag of your name, p tag with description  of yourself, and an unordered list of two truths and a lie.</p>
-      <p>Export, import into App, and mount it.</p>
-      <p>Deadline at 8:05pm</p>
-      <Header /> {/* this is how you mount a component */}
-      <Name />
-      <Footer />
-      <Home />
+
     </div>
-    
-  );
+  )
 }
 
 export default App;
