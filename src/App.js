d30a01573dd8bc328ccf9ab3bb0d4e9ecda933f3
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-02-04 21:29:55 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -4,24 +4,14 @@ import Header from './components/Header'
 import Name from './components/challenges/day01/Name'
 import Footer from './components/challenges/day01/Footer'
 import Home from './components/challenges/day01/Home'
+import MortyParent from './components/Morty/MortyParent';
+import ChuckParent from './components/Chuck/ChuckParent'
 
-function App() { //this is a root component
-  const name = 'Perla' //JS above return statement can be
-                      // injected into JSX using {}
+function App() { 
 
   return (
-    <div className="App">
-      <h1>Welcome to React, {name}</h1>
-      <h2>We just modified our root App Component</h2>
-      <h3>CHALLENGE!</h3>
-      <p>Create ./components/challenges/day 01 folder</p>
-      <p>Create a component with h1 tag of your name, p tag with description  of yourself, and an unordered list of two truths and a lie.</p>
-      <p>Export, import into App, and mount it.</p>
-      <p>Deadline at 8:05pm</p>
-      <Header /> {/* this is how you mount a component */}
-      <Name />
-      <Footer />
-      <Home />
+    <div>
+      <ChuckParent/>
     </div>
     
   );
